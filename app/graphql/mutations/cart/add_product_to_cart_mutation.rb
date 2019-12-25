c11e1fa77663b0f856a9615e3cb29538b2b92f5c
Vinicius Marques <vini.type@gmail.com>
2019-12-25 17:12:03 +0000

    Refactor Cart mutation module/class code style

--- app/graphql/mutations/cart/add_product_to_cart_mutation.rb
+++ app/graphql/mutations/cart/add_product_to_cart_mutation.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-class Mutations::Cart::AddProductToCartMutation < Mutations::BaseMutation
-  argument :products, [String], required: true
+module Mutations
+  module Cart
+    class AddProductToCartMutation < Mutations::BaseMutation
+      argument :products, [String], required: true
 
-  field :cart, Types::CartType, null: false
+      field :cart, Types::CartType, null: false
 
-  def resolve(products:)
-    cart = context[:current_user].cart
-    cart.products.push(Product.find(products))
-    cart.save
+      def resolve(products:)
+        cart = context[:current_user].cart
+        cart.products.push(Product.find(products))
+        cart.save
 
-    { cart: cart }
+        { cart: cart }
+      end
+    end
   end
 end
--- app/graphql/mutations/cart/remove_product_from_cart_mutation.rb
+++ app/graphql/mutations/cart/remove_product_from_cart_mutation.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-class Mutations::Cart::RemoveProductFromCartMutation < Mutations::BaseMutation
-  argument :products, [String], required: true
+module Mutations
+  module Car
+    class RemoveProductFromCartMutation < Mutations::BaseMutation
+      argument :products, [String], required: true
 
-  field :cart, Types::CartType, null: false
+      field :cart, Types::CartType, null: false
 
-  def resolve(products:)
-    cart = context[:current_user].cart
-    cart.products.delete(Product.find(products))
-    cart.save
+      def resolve(products:)
+        cart = context[:current_user].cart
+        cart.products.delete(Product.find(products))
+        cart.save
 
-    { cart: cart }
+        { cart: cart }
+      end
+    end
   end
 end
